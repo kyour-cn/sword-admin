dfb12c0b26d968692dcbb56f33dfd0a44dc3a631
kyour-cn <kyour@outlook.com>
2023-01-10 22:57:57 +0800

    [perf] 完善ws直播服务端

--- process/WsLive.php
+++ process/WsLive.php
@@ -1,16 +1,13 @@
 <?php
 namespace process;
 
-use think\facade\Cache;
 use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Websocket;
 
 class WsLive
 {
-    public $cache = [
-
-    ];
-
     public $puller = [];
+    public $pusher = [];
 
     public function onWebSocketConnect(TcpConnection $connection, $http_buffer)
     {
@@ -22,55 +19,56 @@ class WsLive
 
     public function onMessage(TcpConnection $connection, $res)
     {
-//        var_dump(microtime());
-
-        $userCacheKey = "user_type:{$connection->id}";
-//        $userType = Cache::get($userCacheKey);
-
         //设置用户鉴权
         $action = substr($res, 0, 4);
         $data = substr($res, 5);
 
-//        var_dump($action);
         switch ($action){
             case "logi":
-                $this->cache[$userCacheKey] = $data;
-//                Cache::set($userCacheKey, $data, 86400);
-                var_dump($data);
-                break;
-            case "push":
-//                $this->cache["live"] = $data;
-//                Cache::set("live", $data);
+                if($data == 'push'){
+                    var_dump("注册推");
+                    $this->pusher[$connection->id] = 1;
+                }elseif($data == 'pull'){
+                    var_dump("注册拉");
+                    $connection->websocketType = Websocket::BINARY_TYPE_ARRAYBUFFER;
 
-                //开始推
-                foreach ($this->puller as $id){
-                    if(isset($connection->worker->connections[$id])){
-                        $connection->worker->connections[$id]->send($data);
-                        echo '+';
+                    //告诉推流 上传图像
+                    if(count($this->puller) == 0){
+                        foreach ($this->pusher as $id => $val){
+                            if(isset($connection->worker->connections[$id])){
+                                $connection->worker->connections[$id]->send("push");
+                            }
+                        }
+                    }
+                    $this->puller[$connection->id] = 1;
+                }
+                break;
+            default:
+                $hex = bin2hex($action);
+                if($hex == "ffd8ffe0") {
+//                    echo ".";
+                    foreach ($this->puller as $id => $val){
+                        if(isset($connection->worker->connections[$id])){
+                            $connection->worker->connections[$id]->send($res);
+                        }
+                    }
+                    if(count($this->puller)){
+                        $connection->send("push");
                     }
                 }
-                $connection->send("push");
-                break;
-            case "pull":
-                echo '.';
-                //获取图像
-//                $num = rand(1,5);
-//                $msg = file_get_contents("stream ({$num}).jpg");
-//                $connection->send(base64_encode($msg));
-
-//                $img = Cache::get("live", '');
-//                $img = $this->cache["live"]??'';
-                $this->puller[] = $connection->id;
-                var_dump("注册拉");
-//                $connection->send($img);
                 break;
         }
-//        var_dump(microtime());
-
     }
 
     public function onClose(TcpConnection $connection)
     {
-        echo "onClose\n";
+        if(isset($this->puller[$connection->id])){
+            echo "onClose Pull\n";
+            unset($this->puller[$connection->id]);
+        }
+        if(isset($this->pusher[$connection->id])){
+            echo "onClose Push\n";
+            unset($this->pusher[$connection->id]);
+        }
     }
 }
