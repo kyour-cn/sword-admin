79e94980a8eda1167a4f8e69090f8cebd9544a04
Kyour.cn <kyour@vip.qq.com>
2023-03-30 15:56:01 +0800

    [fix] php8.1 think-cache报错

--- config/thinkcache.php
+++ config/thinkcache.php
@@ -18,7 +18,9 @@ return [
             'password' => env('CACHE_REDIS_PASSWORD', env('REDIS_PASSWORD', '')),
             'select' => env('CACHE_REDIS_DB', env('REDIS_DB', '')),
             'prefix' => env('CACHE_PREFIX', ''),
-            'expire' => env('CACHE_EXPIRE', 0)
+            'expire' => env('CACHE_EXPIRE', 0),
+            //指定序列化方式 -兼容php8.1报错
+            'serialize' => ['serialize', 'unserialize']
         ],
     ],
 ];
