dcf02a72c255a250be0ce31c41b105d63276965d
Kyour.cn <kyour@outlook.com>
2023-08-03 15:43:46 +0800

    perf: 缓存配置的优化

--- config/plugin/sword/cache/app.php
+++ config/plugin/sword/cache/app.php
@@ -3,7 +3,7 @@
 return [
     'enable'  => true,
     // 默认缓存驱动
-    'default' => 'redis',
+    'default' => env('CACHE_TYPE', 'file'),
     // 缓存连接方式配置
     'stores'  => [
         // 文件缓存
@@ -15,7 +15,7 @@ return [
             // 缓存前缀
             'prefix'     => '',
             // 缓存有效期 0表示永久缓存
-            'expire'     => 0,
+            'expire'     => env('CACHE_EXPIRE', 0),
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
@@ -26,18 +26,18 @@ return [
             // 驱动方式
             'type'       => 'redis',
             // 服务器地址
-            'host'       => '127.0.0.1',
+            'host'       => env('CACHE_REDIS_HOST', env('REDIS_HOST', '127.0.0.1')),
             // 服务器端口
-            'port'       => 6379,
+            'port'       => env('CACHE_REDIS_PORT', env('REDIS_PORT', 6379)),
             // 连接密码
-            'password'   => '',
-            'select'     => 0,
+            'password'   => env('CACHE_REDIS_PASSWORD', env('REDIS_PASSWORD', '')),
+            'select'     => env('CACHE_REDIS_DB', env('REDIS_DB', 0)),
             'timeout'    => 0,
             // 缓存有效期 0表示永久缓存
-            'expire'     => 0,
+            'expire'     => env('CACHE_EXPIRE', 0),
             'persistent' => false,
             // 缓存前缀
-            'prefix'     => '',
+            'prefix'     => env('CACHE_PREFIX', ''),
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
         ],
